fe9d3220e98b0425997824edbfee6f7447652b45
shova29 <halima2996@gmail.com>
2022-04-04 20:08:49 +0600

    customer-review updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import HeaderImage from "../../Assets/Images/header-img.png";
 import useReviews from "../../hooks/useReviews";
-import CustomerReviews from "../CustomerReviews/CustomerReviews";
 import ReviewCard from "../ReviewCard/ReviewCard";
 
 const Home = () => {
   const [reviews, setReviews] = useReviews();
+  const navigate = useNavigate();
+  // const x = reviews.slice(0, 3);
   return (
     <div className="bg-white md:container md:mx-auto lg:container">
       <section className="mt-16 flex bg-white">
@@ -30,7 +32,27 @@ const Home = () => {
         </div>
       </section>
       <section className="mt-20">
-        <CustomerReviews></CustomerReviews>
+        <h2 className="mt-8 font-bold text-4xl text-center">
+          Customer Reviews(3)
+        </h2>
+        <div className="mt-8 review-container container mx-auto lg:columns-3 sm:columns-1">
+          {reviews.slice(0, 3).map((reviewCard) => (
+            <ReviewCard
+              key={reviewCard.id}
+              reviewCard={reviewCard}
+            ></ReviewCard>
+          ))}
+        </div>
+        <div className="text-center">
+          {" "}
+          <button
+            onClick={() => navigate("/reviews")}
+            type="button"
+            class="text-center text-xl px-16 py-2 focus:outline-none text-white bg-teal-500 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-900"
+          >
+            See All Reviews
+          </button>
+        </div>
       </section>
     </div>
   );
---
+++ src/hooks/useCharts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useCharts = () => {
+  return <div></div>;
+};
+
+export default useCharts;
